1e0f331434f524c09cdb7aa667a0cce810efc79a
Nico Plyley <nico@phoenixtechlab.com>
2025-04-30 16:13:31 -0700

    :sparkles: Add mobile navigation with menu toggle and responsiveness
    Introduced a responsive mobile navigation menu with a toggle button and slide transition. Adjusted styles for different screen sizes and optimized layout for smaller devices. Ensured menu visibility adapts based on window width and updates dynamically on resize.

--- src/lib/components/navigation/index.svelte
+++ src/lib/components/navigation/index.svelte
@@ -1,11 +1,28 @@
 <script lang="ts">
+	import { slide } from 'svelte/transition';
+	import { browser } from '$app/environment';
+	let menuIsOpen = false;
+
+	const toggleMenu = () => {
+		menuIsOpen = !menuIsOpen;
+	};
+
+	if (browser) {
+		menuIsOpen = window.innerWidth > 1024 && true;
+		window.addEventListener('resize', () => {
+			menuIsOpen = window.innerWidth > 1024 && true;
+		});
+	}
 </script>
 
-<div class="container">
-	<header>
-		<div class="logo">Lee & Stires, Inc.</div>
+<header class="container">
+	<div class="logo">Lee & Stires, Inc.</div>
 
-		<nav>
+	<div class="mobile-nav">
+		<button on:click={toggleMenu}>Menu</button>
+	</div>
+	{#if menuIsOpen}
+		<nav transition:slide>
 			<ul>
 				<li class="active">Home</li>
 				<li>About</li>
@@ -15,16 +32,24 @@
 				<li>Careers</li>
 			</ul>
 		</nav>
-	</header>
-</div>
+	{/if}
+</header>
 
 <style lang="postcss">
 	.logo {
-		@apply mt-10 text-center text-9xl font-bold uppercase text-primary drop-shadow-[5px_10px_10px_#000];
+		@apply mt-10 text-center text-5xl font-bold uppercase text-primary drop-shadow-[5px_10px_10px_#000];
+
+		@screen md {
+			@apply text-9xl;
+		}
 	}
 
 	nav ul {
-		@apply mt-20 flex justify-center gap-20 text-3xl;
+		@apply text-center text-2xl;
+
+		@screen md {
+			@apply mt-20 flex justify-center gap-20 text-3xl;
+		}
 	}
 
 	li.active {
@@ -33,5 +58,13 @@
 
 	.container {
 		@apply mb-20;
+	}
+
+	.mobile-nav button {
+		@apply mt-10 w-full rounded-md bg-secondary p-4 text-white;
+
+		@screen md {
+			@apply hidden;
+		}
 	}
 </style>
