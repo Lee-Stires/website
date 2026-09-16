9d4fd7eb81063fcb9e1d5acfa70b43e3457536ce
Nico Plyley <nico@phoenixtechlab.com>
2026-09-16 15:56:38 -0700

    feat: updated content for use with the lab

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Container } from './Container.astro';
 export { default as Footer } from './Footer.astro';
 export { default as Header } from './Header.astro';
 export { default as Logo } from './Logo.astro';
+export { default as NotFound } from './NotFound.astro';
